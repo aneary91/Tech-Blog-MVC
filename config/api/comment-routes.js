1d1cb90ef42a0d6ee3271c60177aa69fe81f1b66
AllisonWonderland <aneary91@gmail.com>
2021-06-12 20:20:46 -0500

    created the post router

--- config/api/comment-routes.js
+++ config/api/comment-routes.js
@@ -1,4 +1,25 @@
 const router = require('express').Router();
 const { comment } = require('../../models/comment');
-const withAuth = require('../../utils/author');
+const withAuth = require('../../utils/auth');
 
+router.get('/', (req, res) => {
+    Comment.findAll()
+    .then(dbCommentData => res.join(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });  
+});
+
+router.post('/', (req, res) => {
+    Comment.create({ 
+        comment_text: req.body.comment_text,
+        user_id: req.session.user_id,
+        post_id: req.body.post_id
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+});
