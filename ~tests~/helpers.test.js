1c9cc15e1df11654718263484df50e4bd797f93f
AllisonWonderland <aneary91@gmail.com>
2021-06-12 19:27:17 -0500

    created a test method for format_plural and format_date

--- ~tests~/helpers.test.js
+++ ~tests~/helpers.test.js
@@ -11,3 +11,16 @@ test('format_url() returns a simplified url string', () => {
 
 });
 
+test('format_plural() returns a pluralized word', () => {
+    const word1 = format_plural('tiger', '1');
+    const word2 = format_plural('lion', '2');
+
+    expect(word1).toBe('tiger');
+    expect(word2).toBe('lion');
+});
+
+test('format_date() returns a date string', () => {
+    const date = new Date('2021-06-03');
+
+    expect(format_date(date)).toBe('6-3-2021');
+});
