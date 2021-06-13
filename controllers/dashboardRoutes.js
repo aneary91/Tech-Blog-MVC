519601c7d4cf0dd6f1e80ab993dd3fcb5aed0a70
AllisonWonderland <aneary91@gmail.com>
2021-06-12 21:12:28 -0500

    finished the get router

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -11,12 +11,35 @@ router.get('/', withaAuth, (req, res) => {
         where: {
             user_id: req.session.user_id
         },
-        attributes: {
+        attributes: [
+
             'id', 
             'post_url', 
             'title', 
             'created_at',
-            [sequelize.literal('SELECT COUNT(*)FROM vote WHERE post.id = vote.post_id'), 'vote_count']
-        }
+            [sequelize.literal('SELECT COUNT(*)FROM vote WHERE post.id = vote.post_id'), 'vote_count'],
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
     })
-})
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true}));
+        res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
