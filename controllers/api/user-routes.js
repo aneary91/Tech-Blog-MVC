0a0f2c98c8abd5b05e57460e14fd3f5e15b9b2df
AllisonWonderland <aneary91@gmail.com>
2021-06-15 09:00:12 -0500

    created a new folder for utils and added an auth.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -140,8 +140,9 @@ router.delete("/:id", (req, res) => {
   User.destroy({
     where: {
       id: req.params.id,
-    },
-  }).then((dbUserData) => {
+    }
+  })
+  .then(dbUserData => {
     if (!dbUserData) {
       res.status(404).json({ message: "No user found with this id" });
       return;
