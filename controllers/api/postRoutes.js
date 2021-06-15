2ff712c21fddde37db67d8c1252167a53060acaf
AllisonWonderland <aneary91@gmail.com>
2021-06-14 23:12:41 -0500

    created the delete router

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -99,4 +99,48 @@ router.put('/upvote', withAuth, (req, res) => {
     });
 });
 
+router.put('/:id', withAuth, (req, res) => {
+    Post.update(
+        {
+            title: req.body.title
+        },
+        {
+            where: {
+                id: req.params.is
+            }
+        }
+    )
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+    console.log('id', req.params.id);
+    Post.destroy({ 
+        where: { 
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        if(!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    });
+});
+
 module.exports = router;
