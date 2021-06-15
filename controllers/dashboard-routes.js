527e72721cea19ecd2025b395eeaf82531853e67
AllisonWonderland <aneary91@gmail.com>
2021-06-15 09:10:47 -0500

    created a get all posts for dashboard

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const sequelize = require('../config/connections');
+const { Post, User, Comment, Vote } = require('../models');
+const withAuth = require('../utils/auth');
+
+// get all posts for dashboard
+router.get('/', withAuth, (req, res) => {
+    console.log(req.session);
+    console.log('================================');
+    Post.findAll({ 
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']   
+        ],
+        inclue: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: USer,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
