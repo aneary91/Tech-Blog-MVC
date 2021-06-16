7a733d205cce0e622ad6d2433391f390443ffeb5
AllisonWonderland <aneary91@gmail.com>
2021-06-16 11:34:35 -0500

    finished the public folder with all the differnt handlers also added more seed files...

--- seeds/index.js
+++ seeds/index.js
@@ -1,16 +1,26 @@
-const sequelize = require('../config/connection');
-const Dish = require('../models/Dish');
-const dishData = require('./dish-seeds.json');
+const seedUsers = require('./user-seeds');
+const seedPosts = require('./post-seeds');
+const seedComments = require('./comment-seeds');
+const seedVotes = require('./vote-seeds');
 
-const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
 
-  await Dish.bulkCreate(dishData, {
-    individualHooks: true,
-    returning: true,
-  });
 
-  process.exit(0);
-};
 
-seedDatabase();
+
+
+
+
+
+
+// const seedDatabase = async () => {
+//   await sequelize.sync({ force: true });
+
+//   await Dish.bulkCreate(dishData, {
+//     individualHooks: true,
+//     returning: true,
+//   });
+
+//   process.exit(0);
+// };
+
+seedAll();
