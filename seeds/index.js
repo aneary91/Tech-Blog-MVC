6084984710a29ef1d0998eae3f9a339dd613228a
AllisonWonderland <aneary91@gmail.com>
2021-06-16 12:56:41 -0500

    finished the index seed

--- seeds/index.js
+++ seeds/index.js
@@ -3,24 +3,24 @@ const seedPosts = require('./post-seeds');
 const seedComments = require('./comment-seeds');
 const seedVotes = require('./vote-seeds');
 
+const sequelize = require('../config/connections');
 
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('--------------');
+  await seedUsers();
+  console.log('--------------');
 
+  await seedPosts();
+  console.log('--------------');
 
+  await seedComments();
+  console.log('--------------');
 
+  await seedVotes();
+  console.log('--------------');
 
-
-
-
-
-// const seedDatabase = async () => {
-//   await sequelize.sync({ force: true });
-
-//   await Dish.bulkCreate(dishData, {
-//     individualHooks: true,
-//     returning: true,
-//   });
-
-//   process.exit(0);
-// };
+  process.exit(0);
+};
 
 seedAll();
